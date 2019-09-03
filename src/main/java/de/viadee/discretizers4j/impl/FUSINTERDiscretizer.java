f0f3c540b5ead4f3a13dfa58ef69d995d8d1b9a7
Frank Köhne <frank.koehne@viadee.de>
2019-09-03 16:35:29 +0200

    Test performance issue in #1
    Could not reproduce the long runtimes seen in the chart.
    Included some obvious, but minor perfomance optimizations anyway.
    Code formatting.

--- src/main/java/de/viadee/discretizers4j/impl/FUSINTERDiscretizer.java
+++ src/main/java/de/viadee/discretizers4j/impl/FUSINTERDiscretizer.java
@@ -1,137 +1,144 @@
 package de.viadee.discretizers4j.impl;
 
-import de.viadee.discretizers4j.AbstractSupervisedDiscretizer;
-import de.viadee.discretizers4j.DiscretizationTransition;
-import de.viadee.discretizers4j.Interval;
-
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.viadee.discretizers4j.AbstractSupervisedDiscretizer;
+import de.viadee.discretizers4j.DiscretizationTransition;
+import de.viadee.discretizers4j.Interval;
+
 /**
- * Implementation of the FUSINTER discretization algorithm described by [Zighed, Rabaséda, Rakotomalala 1998]
+ * Implementation of the FUSINTER discretization algorithm described by [Zighed,
+ * Rabaséda, Rakotomalala 1998]
  */
 public class FUSINTERDiscretizer extends AbstractSupervisedDiscretizer {
-    private final double lambda;
-    private final double alpha;
+	private final double lambda;
+	private final double alpha;
 
-    // Number of possible classifications
-    private int m;
-    // Number of instances
-    private int n;
+	/** Number of possible classifications */
+	private int m;
 
-    /**
-     * Generates a FUSINTER discretizer with parameters suggested by the authors
-     */
-    public FUSINTERDiscretizer() {
-        this(1.0, 0.975);
-    }
+	/** Number of instances */
+	private int n;
 
-    /**
-     * Generates a FUSINTER discretizer with custom parameters
-     *
-     * @param lambda lambda value
-     * @param alpha  alpha value
-     */
-    public FUSINTERDiscretizer(double lambda, double alpha) {
-        super(true);
-        this.lambda = lambda;
-        this.alpha = alpha;
-    }
+	/**
+	 * Generates a FUSINTER discretizer with parameters suggested by the authors
+	 */
+	public FUSINTERDiscretizer() {
+		this(1.0, 0.975);
+	}
 
-    /**
-     * Implementation of FUSINTER, 1. sort, 2. equalClassIntervals 3. merge if entropy improves
-     *
-     * @param keyValuePairs List of Values and Labels
-     * @return list of Intervals determined to have the highest entropy
-     */
-    @Override
-    protected List<DiscretizationTransition> fitCreateTransitions(List<AbstractMap.SimpleImmutableEntry<Double, Double>> keyValuePairs) {
+	/**
+	 * Generates a FUSINTER discretizer with custom parameters
+	 *
+	 * @param lambda lambda value
+	 * @param alpha  alpha value
+	 */
+	public FUSINTERDiscretizer(double lambda, double alpha) {
+		super(true);
+		this.lambda = lambda;
+		this.alpha = alpha;
+	}
 
-        m = keyValuePairs.stream().map(AbstractMap.SimpleImmutableEntry::getValue).sorted().distinct().toArray().length;
-        n = keyValuePairs.size();
+	/**
+	 * Implementation of FUSINTER, 1. sort, 2. equalClassIntervals 3. merge if
+	 * entropy improves
+	 *
+	 * @param keyValuePairs List of Values and Labels
+	 * @return list of Intervals determined to have the highest entropy
+	 */
+	@Override
+	protected List<DiscretizationTransition> fitCreateTransitions(
+			List<AbstractMap.SimpleImmutableEntry<Double, Double>> keyValuePairs) {
 
-        List<Interval> equalClassSplits = equalClassSplit(keyValuePairs);
-        List<Interval> evaluatedIntervals = evaluateIntervals(equalClassSplits, keyValuePairs);
-        List<Double> actualCutPoints = new ArrayList<>();
-        for (int i = 0; i < evaluatedIntervals.size() - 1; i++) {
-            actualCutPoints.add(
-                    (keyValuePairs.get(evaluatedIntervals.get(i).getEnd()).getKey()
-                            + keyValuePairs.get(evaluatedIntervals.get(i + 1).getBegin()).getKey())
-                            / 2D);
-        }
+		m = keyValuePairs.stream().map(AbstractMap.SimpleImmutableEntry::getValue).sorted().distinct().toArray().length;
+		n = keyValuePairs.size();
 
-        return getDiscretizationTransitionsFromCutPoints(actualCutPoints, keyValuePairs.get(0).getKey(), keyValuePairs.get(keyValuePairs.size() - 1).getKey());
-    }
+		final List<Interval> equalClassSplits = equalClassSplit(keyValuePairs);
+		final List<Interval> evaluatedIntervals = evaluateIntervals(equalClassSplits, keyValuePairs);
+		final List<Double> actualCutPoints = new ArrayList<>(100);
+		for (int i = 0; i < evaluatedIntervals.size() - 1; i++) {
+			actualCutPoints.add((keyValuePairs.get(evaluatedIntervals.get(i).getEnd()).getKey()
+					+ keyValuePairs.get(evaluatedIntervals.get(i + 1).getBegin()).getKey()) / 2D);
+		}
 
-    private List<Interval> evaluateIntervals(List<Interval> equalClassSplits,
-                                             final List<AbstractMap.SimpleImmutableEntry<Double, Double>> keyValuePairs) {
-        boolean improvement = true;
+		return getDiscretizationTransitionsFromCutPoints(actualCutPoints, keyValuePairs.get(0).getKey(),
+				keyValuePairs.get(keyValuePairs.size() - 1).getKey());
+	}
 
-        while (equalClassSplits.size() > 1 && improvement) {
-            int deleteIndex = 0;
-            double maxMergeCriterion = 0.0;
-            double oldCriterion = determineDiscretizationCriterion(equalClassSplits);
-            for (int i = 0; i < equalClassSplits.size() - 1; i++) {
-                List<Interval> possibleMergedInterval = mergeInterval(equalClassSplits, i, keyValuePairs);
-                final double mergeCriterion = oldCriterion - determineDiscretizationCriterion(possibleMergedInterval);
-                if (mergeCriterion > maxMergeCriterion) {
-                    deleteIndex = i;
-                    maxMergeCriterion = mergeCriterion;
-                }
-            }
+	private List<Interval> evaluateIntervals(List<Interval> equalClassSplits,
+			final List<AbstractMap.SimpleImmutableEntry<Double, Double>> keyValuePairs) {
+		boolean improvement = true;
 
-            if (maxMergeCriterion > 0) {
-                equalClassSplits = mergeInterval(equalClassSplits, deleteIndex, keyValuePairs);
-            } else {
-                improvement = false;
-            }
-        }
+		while (equalClassSplits.size() > 1 && improvement) {
+			int deleteIndex = 0;
+			double maxMergeCriterion = 0.0;
+			final double oldCriterion = determineDiscretizationCriterion(equalClassSplits);
+			for (int i = 0; i < equalClassSplits.size() - 1; i++) {
+				final List<Interval> possibleMergedInterval = mergeInterval(equalClassSplits, i, keyValuePairs);
+				final double mergeCriterion = oldCriterion - determineDiscretizationCriterion(possibleMergedInterval);
+				if (mergeCriterion > maxMergeCriterion) {
+					deleteIndex = i;
+					maxMergeCriterion = mergeCriterion;
+				}
+			}
 
-        return equalClassSplits;
-    }
+			if (maxMergeCriterion > 0) {
+				equalClassSplits = mergeInterval(equalClassSplits, deleteIndex, keyValuePairs);
+			} else {
+				improvement = false;
+			}
+		}
 
-    /**
-     * determines the Entropy of the given Intervals with the quadratic entropy formula
-     *
-     * @param intervals to be evaluated
-     * @return double from 0 to 1 with 0 being perfect entropy
-     */
-    private double determineDiscretizationCriterion(List<Interval> intervals) {
-        double criterion = 0;
-        for (Interval interval : intervals) {
-            double intervalClassSum = 0;
-            for (int i = 0; i < m; i++) {
-                double quotient = (interval.getClassDist()[i] + lambda) / (interval.getSize() + m * lambda);
-                intervalClassSum += (quotient * (1 - quotient));
-            }
-            double intervalSum = alpha * ((interval.getSize()) / (double) n) * intervalClassSum;
-            intervalSum += ((1 - alpha) * ((m * lambda) / (interval.getSize())));
+		return equalClassSplits;
+	}
 
-            criterion += intervalSum;
-        }
+	/**
+	 * determines the Entropy of the given Intervals with the quadratic entropy
+	 * formula
+	 *
+	 * @param intervals to be evaluated
+	 * @return double from 0 to 1 with 0 being perfect entropy
+	 */
+	private double determineDiscretizationCriterion(List<Interval> intervals) {
+		double criterion = 0;
+		double quotient = 0;
+		double intervalClassSum;
+		final double mTimesLambda = m * lambda;
+		for (final Interval interval : intervals) {
+			intervalClassSum = 0;
+			final int intervalsize = interval.getSize();
+			for (int i = 0; i < m; i++) {
+				quotient = (interval.getClassDist()[i] + lambda) / (intervalsize + mTimesLambda);
+				intervalClassSum += (quotient * (1 - quotient));
+			}
+			double intervalSum = alpha * (intervalsize / (double) n) * intervalClassSum;
+			intervalSum += ((1 - alpha) * ((mTimesLambda) / intervalsize));
+			criterion += intervalSum;
+		}
 
-        return criterion;
-    }
+		return criterion;
+	}
 
-    /**
-     * merges Interval i and i+1,
-     *
-     * @param intervals     list of Interval to be reduced by one Element
-     * @param i             index of element that will be merged
-     * @param keyValuePairs list of values used for index
-     * @return new List with Interval i and i+1 merged
-     */
-    private List<Interval> mergeInterval(List<Interval> intervals, int i,
-                                         final List<AbstractMap.SimpleImmutableEntry<Double, Double>> keyValuePairs) {
-        List<Interval> temp = new ArrayList<>(intervals.subList(0, intervals.size()));
-        int mergeBegin = temp.get(i).getBegin();
-        int mergeEnd = temp.get(i + 1).getEnd();
-        temp.add(i, new Interval(mergeBegin, mergeEnd, keyValuePairs));
-        temp.remove(i + 1);
-        temp.remove(i + 1);
+	/**
+	 * merges Interval i and i+1,
+	 *
+	 * @param intervals     list of Interval to be reduced by one Element
+	 * @param i             index of element that will be merged
+	 * @param keyValuePairs list of values used for index
+	 * @return new List with Interval i and i+1 merged
+	 */
+	private List<Interval> mergeInterval(List<Interval> intervals, int i,
+			final List<AbstractMap.SimpleImmutableEntry<Double, Double>> keyValuePairs) {
+		final List<Interval> temp = new ArrayList<>(intervals.subList(0, intervals.size()));
+		final int mergeBegin = temp.get(i).getBegin();
+		final int mergeEnd = temp.get(i + 1).getEnd();
+		temp.add(i, new Interval(mergeBegin, mergeEnd, keyValuePairs));
+		temp.remove(i + 1);
+		temp.remove(i + 1);
 
-        return temp;
-    }
+		return temp;
+	}
 
 }
--- src/test/java/de/viadee/discretizers4j/impl/FUSINTERDiscretizerTest.java
+++ src/test/java/de/viadee/discretizers4j/impl/FUSINTERDiscretizerTest.java
@@ -1,252 +1,161 @@
 package de.viadee.discretizers4j.impl;
 
-import de.viadee.discretizers4j.DiscretizationTransition;
-import de.viadee.discretizers4j.NumericDiscretizationOrigin;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import de.viadee.discretizers4j.DiscretizationTransition;
+import de.viadee.discretizers4j.NumericDiscretizationOrigin;
 
 class FUSINTERDiscretizerTest {
 
-    /**
-     * Tests Steps 4-8 (Evaluation of potential merges of the equalClassSplits) of the described Algorithm,
-     * Number[][] to be discretized is taken from FUSINTER paper Figure 7.
-     */
-    @Test
-    void testIntervalReduction() {
-        Number[][] values = new Number[][]{
-                {1.0, 1},
-                {2.0, 0},
-                {3.0, 1},
-                {4.0, 1},
-                {5.0, 1},
-                {6.0, 1},
-                {7.0, 1},
-                {8.0, 1},
-                {9.0, 1},
-                {10.0, 1},
-                {11.0, 1},
-                {12.0, 1},
-                {13.0, 1},
-                {14.0, 1},
-                {15.0, 0},
-                {16.0, 1},
-                {17.0, 0},
-                {18.0, 1},
-                {19.0, 0},
-                {20.0, 1},
-                {21.0, 0},
-                {22.0, 1},
-                {23.0, 0},
-                {24.0, 1},
-                {25.0, 0},
-                {26.0, 1},
-                {27.0, 0},
-                {28.0, 0},
-                {29.0, 0},
-                {30.0, 0},
-                {31.0, 0},
-                {32.0, 0},
-                {33.0, 0},
-                {34.0, 0},
-                {35.0, 0},
-                {36.0, 0},
-                {37.0, 0},
-                {38.0, 1},
-                {39.0, 0},
-                {40.0, 1},
-        };
-        Serializable[] serializables = new Serializable[values.length];
-        Double[] doubles = new Double[values.length];
-        for (int i = 0; i < values.length; i++) {
-            serializables[i] = values[i][0];
-            doubles[i] = values[i][1].doubleValue();
-        }
+	/**
+	 * Tests Steps 4-8 (Evaluation of potential merges of the equalClassSplits) of
+	 * the described Algorithm, Number[][] to be discretized is taken from FUSINTER
+	 * paper Figure 7.
+	 */
+	@Test
+	void testIntervalReduction() {
+		final Number[][] values = new Number[][] { { 1.0, 1 }, { 2.0, 0 }, { 3.0, 1 }, { 4.0, 1 }, { 5.0, 1 },
+				{ 6.0, 1 }, { 7.0, 1 }, { 8.0, 1 }, { 9.0, 1 }, { 10.0, 1 }, { 11.0, 1 }, { 12.0, 1 }, { 13.0, 1 },
+				{ 14.0, 1 }, { 15.0, 0 }, { 16.0, 1 }, { 17.0, 0 }, { 18.0, 1 }, { 19.0, 0 }, { 20.0, 1 }, { 21.0, 0 },
+				{ 22.0, 1 }, { 23.0, 0 }, { 24.0, 1 }, { 25.0, 0 }, { 26.0, 1 }, { 27.0, 0 }, { 28.0, 0 }, { 29.0, 0 },
+				{ 30.0, 0 }, { 31.0, 0 }, { 32.0, 0 }, { 33.0, 0 }, { 34.0, 0 }, { 35.0, 0 }, { 36.0, 0 }, { 37.0, 0 },
+				{ 38.0, 1 }, { 39.0, 0 }, { 40.0, 1 }, };
+		final Serializable[] serializables = new Serializable[values.length];
+		final Double[] doubles = new Double[values.length];
+		for (int i = 0; i < values.length; i++) {
+			serializables[i] = values[i][0];
+			doubles[i] = values[i][1].doubleValue();
+		}
 
-        FUSINTERDiscretizer fusinterDiscretizer = new FUSINTERDiscretizer();
-        fusinterDiscretizer.fit(serializables, doubles);
-        List<DiscretizationTransition> list = new ArrayList<>(fusinterDiscretizer.getTransitions());
-        assertEquals(4, list.size());
-    }
+		final FUSINTERDiscretizer fusinterDiscretizer = new FUSINTERDiscretizer();
+		fusinterDiscretizer.fit(serializables, doubles);
+		final List<DiscretizationTransition> list = new ArrayList<>(fusinterDiscretizer.getTransitions());
+		assertEquals(4, list.size());
+	}
 
+	/**
+	 * Tests Steps 4-8 (Evaluation of potential merges of the equalClassSplits) of
+	 * the described Algorithm, Number[][] to be discretized is taken from FUSINTER
+	 * paper Figure 5 and 6.
+	 */
+	@Test
+	void testIntervalReductionBig() {
+		final Number[][] values = new Number[][] { { 1.0, 1 }, { 2.0, 0 }, { 3.0, 1 }, { 3.0, 1 }, { 4.0, 1 },
+				{ 4.0, 1 }, { 5.0, 1 }, { 5.0, 1 }, { 5.0, 1 }, { 6.0, 1 }, { 6.0, 1 }, { 6.0, 1 }, { 7.0, 1 },
+				{ 7.0, 1 }, { 7.0, 1 }, { 8.0, 1 }, { 8.0, 1 }, { 9.0, 1 }, { 9.0, 1 }, { 9.0, 1 }, { 10.0, 1 },
+				{ 10.0, 1 }, { 10.0, 1 }, { 11.0, 1 }, { 11.0, 1 }, { 11.0, 1 }, { 12.0, 1 }, { 12.0, 1 }, { 13.0, 1 },
+				{ 13.0, 1 }, { 13.0, 0 }, { 14.0, 1 }, { 14.0, 1 }, { 14.0, 1 }, { 15.0, 0 }, { 15.0, 0 }, { 15.0, 0 },
+				{ 16.0, 1 }, { 16.0, 1 }, { 16.0, 1 }, { 17.0, 0 }, { 17.0, 0 }, { 17.0, 0 }, { 18.0, 1 }, { 18.0, 1 },
+				{ 18.0, 0 }, { 19.0, 0 }, { 19.0, 0 }, { 20.0, 1 }, { 20.0, 1 }, { 22.0, 1 }, { 22.0, 1 }, { 22.0, 1 },
+				{ 23.0, 0 }, { 23.0, 0 }, { 23.0, 0 }, { 27.0, 0 }, { 27.0, 0 }, { 28.0, 0 }, { 28.0, 0 }, { 28.0, 0 },
+				{ 29.0, 0 }, { 29.0, 0 }, { 29.0, 0 }, { 30.0, 0 }, { 30.0, 0 }, { 30.0, 0 }, { 31.0, 0 }, { 31.0, 0 },
+				{ 31.0, 0 }, { 33.0, 0 }, { 34.0, 0 }, { 34.0, 0 }, { 34.0, 0 }, { 35.0, 0 }, { 35.0, 0 }, { 35.0, 0 },
+				{ 36.0, 0 }, { 36.0, 0 }, { 36.0, 0 }, { 37.0, 0 }, { 37.0, 0 }, { 37.0, 1 }, { 38.0, 1 }, { 38.0, 1 },
+				{ 38.0, 0 }, { 39.0, 0 }, { 39.0, 0 }, { 40.0, 1 }, { 40.0, 1 }, };
+		final Serializable[] serializables = new Serializable[values.length];
+		final Double[] doubles = new Double[values.length];
+		for (int i = 0; i < values.length; i++) {
+			serializables[i] = values[i][0];
+			doubles[i] = values[i][1].doubleValue();
+		}
 
-    /**
-     * Tests Steps 4-8 (Evaluation of potential merges of the equalClassSplits) of the described Algorithm,
-     * Number[][] to be discretized is taken from FUSINTER paper Figure 5 and 6.
-     */
-    @Test
-    void testIntervalReductionBig() {
-        Number[][] values = new Number[][]{
-                {1.0, 1},
-                {2.0, 0},
-                {3.0, 1},
-                {3.0, 1},
-                {4.0, 1},
-                {4.0, 1},
-                {5.0, 1},
-                {5.0, 1},
-                {5.0, 1},
-                {6.0, 1},
-                {6.0, 1},
-                {6.0, 1},
-                {7.0, 1},
-                {7.0, 1},
-                {7.0, 1},
-                {8.0, 1},
-                {8.0, 1},
-                {9.0, 1},
-                {9.0, 1},
-                {9.0, 1},
-                {10.0, 1},
-                {10.0, 1},
-                {10.0, 1},
-                {11.0, 1},
-                {11.0, 1},
-                {11.0, 1},
-                {12.0, 1},
-                {12.0, 1},
-                {13.0, 1},
-                {13.0, 1},
-                {13.0, 0},
-                {14.0, 1},
-                {14.0, 1},
-                {14.0, 1},
-                {15.0, 0},
-                {15.0, 0},
-                {15.0, 0},
-                {16.0, 1},
-                {16.0, 1},
-                {16.0, 1},
-                {17.0, 0},
-                {17.0, 0},
-                {17.0, 0},
-                {18.0, 1},
-                {18.0, 1},
-                {18.0, 0},
-                {19.0, 0},
-                {19.0, 0},
-                {20.0, 1},
-                {20.0, 1},
-                {22.0, 1},
-                {22.0, 1},
-                {22.0, 1},
-                {23.0, 0},
-                {23.0, 0},
-                {23.0, 0},
-                {27.0, 0},
-                {27.0, 0},
-                {28.0, 0},
-                {28.0, 0},
-                {28.0, 0},
-                {29.0, 0},
-                {29.0, 0},
-                {29.0, 0},
-                {30.0, 0},
-                {30.0, 0},
-                {30.0, 0},
-                {31.0, 0},
-                {31.0, 0},
-                {31.0, 0},
-                {33.0, 0},
-                {34.0, 0},
-                {34.0, 0},
-                {34.0, 0},
-                {35.0, 0},
-                {35.0, 0},
-                {35.0, 0},
-                {36.0, 0},
-                {36.0, 0},
-                {36.0, 0},
-                {37.0, 0},
-                {37.0, 0},
-                {37.0, 1},
-                {38.0, 1},
-                {38.0, 1},
-                {38.0, 0},
-                {39.0, 0},
-                {39.0, 0},
-                {40.0, 1},
-                {40.0, 1},
-        };
-        Serializable[] serializables = new Serializable[values.length];
-        Double[] doubles = new Double[values.length];
-        for (int i = 0; i < values.length; i++) {
-            serializables[i] = values[i][0];
-            doubles[i] = values[i][1].doubleValue();
-        }
+		final FUSINTERDiscretizer fusinterDiscretizer = new FUSINTERDiscretizer();
+		fusinterDiscretizer.fit(serializables, doubles);
 
-        FUSINTERDiscretizer fusinterDiscretizer = new FUSINTERDiscretizer();
-        fusinterDiscretizer.fit(serializables, doubles);
+		final List<DiscretizationTransition> list = new ArrayList<>(fusinterDiscretizer.getTransitions());
 
-        List<DiscretizationTransition> list = new ArrayList<>(fusinterDiscretizer.getTransitions());
+		final NumericDiscretizationOrigin discOrigin = ((NumericDiscretizationOrigin) list.get(0)
+				.getDiscretizationOrigin());
+		assertEquals(1D, discOrigin.getMinValue());
+		assertEquals(14.5, discOrigin.getMaxValue());
+		assertEquals(0D, list.get(0).getDiscretizedValue().doubleValue());
+		assertTrue(discOrigin.isFirst());
+		assertFalse(discOrigin.isLast());
 
-        NumericDiscretizationOrigin discOrigin = ((NumericDiscretizationOrigin) list.get(0).getDiscretizationOrigin());
-        assertEquals(1D, discOrigin.getMinValue());
-        assertEquals(14.5, discOrigin.getMaxValue());
-        assertEquals(0D, list.get(0).getDiscretizedValue().doubleValue());
-        assertTrue(discOrigin.isFirst());
-        assertFalse(discOrigin.isLast());
+		final NumericDiscretizationOrigin discOrigin1 = ((NumericDiscretizationOrigin) list.get(1)
+				.getDiscretizationOrigin());
+		assertEquals(14.5, discOrigin1.getMinValue());
+		assertEquals(19.5, discOrigin1.getMaxValue());
+		assertEquals(1D, list.get(1).getDiscretizedValue().doubleValue());
+		assertFalse(discOrigin1.isFirst());
+		assertFalse(discOrigin1.isLast());
 
-        NumericDiscretizationOrigin discOrigin1 = ((NumericDiscretizationOrigin) list.get(1).getDiscretizationOrigin());
-        assertEquals(14.5, discOrigin1.getMinValue());
-        assertEquals(19.5, discOrigin1.getMaxValue());
-        assertEquals(1D, list.get(1).getDiscretizedValue().doubleValue());
-        assertFalse(discOrigin1.isFirst());
-        assertFalse(discOrigin1.isLast());
+		final NumericDiscretizationOrigin discOrigin2 = ((NumericDiscretizationOrigin) list.get(2)
+				.getDiscretizationOrigin());
+		assertEquals(19.5, discOrigin2.getMinValue());
+		assertEquals(22.5, discOrigin2.getMaxValue());
+		assertEquals(2D, list.get(2).getDiscretizedValue().doubleValue());
+		assertFalse(discOrigin2.isFirst());
+		assertFalse(discOrigin2.isLast());
 
-        NumericDiscretizationOrigin discOrigin2 = ((NumericDiscretizationOrigin) list.get(2).getDiscretizationOrigin());
-        assertEquals(19.5, discOrigin2.getMinValue());
-        assertEquals(22.5, discOrigin2.getMaxValue());
-        assertEquals(2D, list.get(2).getDiscretizedValue().doubleValue());
-        assertFalse(discOrigin2.isFirst());
-        assertFalse(discOrigin2.isLast());
+		final NumericDiscretizationOrigin discOrigin3 = ((NumericDiscretizationOrigin) list.get(3)
+				.getDiscretizationOrigin());
+		assertEquals(22.5, discOrigin3.getMinValue());
+		assertEquals(36.5, discOrigin3.getMaxValue());
+		assertEquals(3D, list.get(3).getDiscretizedValue().doubleValue());
+		assertFalse(discOrigin3.isFirst());
+		assertFalse(discOrigin3.isLast());
 
-        NumericDiscretizationOrigin discOrigin3 = ((NumericDiscretizationOrigin) list.get(3).getDiscretizationOrigin());
-        assertEquals(22.5, discOrigin3.getMinValue());
-        assertEquals(36.5, discOrigin3.getMaxValue());
-        assertEquals(3D, list.get(3).getDiscretizedValue().doubleValue());
-        assertFalse(discOrigin3.isFirst());
-        assertFalse(discOrigin3.isLast());
+		final NumericDiscretizationOrigin discOrigin4 = ((NumericDiscretizationOrigin) list.get(4)
+				.getDiscretizationOrigin());
+		assertEquals(36.5, discOrigin4.getMinValue());
+		assertEquals(40D, discOrigin4.getMaxValue());
+		assertEquals(4D, list.get(4).getDiscretizedValue().doubleValue());
+		assertFalse(discOrigin4.isFirst());
+		assertTrue(discOrigin4.isLast());
 
-        NumericDiscretizationOrigin discOrigin4 = ((NumericDiscretizationOrigin) list.get(4).getDiscretizationOrigin());
-        assertEquals(36.5, discOrigin4.getMinValue());
-        assertEquals(40D, discOrigin4.getMaxValue());
-        assertEquals(4D, list.get(4).getDiscretizedValue().doubleValue());
-        assertFalse(discOrigin4.isFirst());
-        assertTrue(discOrigin4.isLast());
+	}
 
-    }
+	/**
+	 * Tests Steps 4-8 (Evaluation of potential merges of the equalClassSplits) of
+	 * the described Algorithm, Number[][] to be discretized is taken from FUSINTER
+	 * paper Figure 7.
+	 */
+	@Test
+	void testEqualDistribution() {
+		final Number[][] values = new Number[][] { { 1.0, 1 }, { 1.0, 0 }, { 2.0, 1 }, { 2.0, 0 }, { 3.0, 1 },
+				{ 3.0, 0 }, { 4.0, 1 }, { 4.0, 0 } };
+		final Serializable[] serializables = new Serializable[values.length];
+		final Double[] doubles = new Double[values.length];
+		for (int i = 0; i < values.length; i++) {
+			serializables[i] = values[i][0];
+			doubles[i] = values[i][1].doubleValue();
+		}
 
-    /**
-     * Tests Steps 4-8 (Evaluation of potential merges of the equalClassSplits) of the described Algorithm,
-     * Number[][] to be discretized is taken from FUSINTER paper Figure 7.
-     */
-    @Test
-    void testEqualDistribution() {
-        Number[][] values = new Number[][]{
-                {1.0, 1},
-                {1.0, 0},
-                {2.0, 1},
-                {2.0, 0},
-                {3.0, 1},
-                {3.0, 0},
-                {4.0, 1},
-                {4.0, 0}
-        };
-        Serializable[] serializables = new Serializable[values.length];
-        Double[] doubles = new Double[values.length];
-        for (int i = 0; i < values.length; i++) {
-            serializables[i] = values[i][0];
-            doubles[i] = values[i][1].doubleValue();
-        }
+		final FUSINTERDiscretizer fusinterDiscretizer = new FUSINTERDiscretizer();
+		fusinterDiscretizer.fit(serializables, doubles);
+		final List<DiscretizationTransition> list = new ArrayList<>(fusinterDiscretizer.getTransitions());
+		assertEquals(1, list.size());
+	}
 
-        FUSINTERDiscretizer fusinterDiscretizer = new FUSINTERDiscretizer();
-        fusinterDiscretizer.fit(serializables, doubles);
-        List<DiscretizationTransition> list = new ArrayList<>(fusinterDiscretizer.getTransitions());
-        assertEquals(1, list.size());
-    }
+	/**
+	 * Tests the performance with a larger dataset. Benchmark Runtime (Laptop) is
+	 * 0.286 s.
+	 */
+	@Test
+	void testFUSINTERPerformance() {
+		final Number[][] saltValues = new Number[][] { { 1.0, 1 }, { 1.0, 0 }, { 2.0, 1 }, { 2.0, 0 }, { 3.0, 1 },
+				{ 3.0, 0 }, { 4.0, 1 }, { 4.0, 0 } };
+		final int dataMultiplicator = 50000; // blow up the dataset
+		final Serializable[] serializables = new Serializable[saltValues.length * dataMultiplicator];
+		final Double[] doubles = new Double[saltValues.length * dataMultiplicator];
+		for (int i = 0; i < saltValues.length * dataMultiplicator; i++) {
+			serializables[i] = saltValues[i % saltValues.length][0];
+			doubles[i] = saltValues[i % saltValues.length][1].doubleValue();
+		}
+
+		final FUSINTERDiscretizer fusinterDiscretizer = new FUSINTERDiscretizer();
+		fusinterDiscretizer.fit(serializables, doubles);
+		final List<DiscretizationTransition> list = new ArrayList<>(fusinterDiscretizer.getTransitions());
+		assertEquals(1, list.size());
+	}
+
 }
